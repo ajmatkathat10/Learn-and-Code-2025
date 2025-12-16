0d6cbdc6eba938903b7b5971e78f15c18132831e
Ajmat Kathat <ajmatkathat@Ajmat-MacBook-Pro.local>
2025-12-16 16:47:44 +0530

    Completed Learn and code Assignment 1

---
+++ assignment1/countryCodeToNamesConsoleApp/index.ts
@@ -0,0 +1,36 @@
+import * as readline from 'readline';
+import { codeToCountryNames } from './countryCodesToNames';
+
+function getFullCountryName(countryCode: string): void {
+  const code = countryCode.toUpperCase();
+  const countryName = codeToCountryNames[code];
+  if (!countryName) console.log(`Country code "${countryCode}" does not exist.`);
+  else console.log(`Country with (${code}) code is ${countryName}`);
+}
+
+function main(): void {
+    const readLine = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+  
+    function takeUserChoice(): void {
+        readLine.question('Enter a country code like IN, US to get full country name or "quit" to quit: ', (input) => {
+            const trimmedUserInput = input.trim();
+            if (trimmedUserInput.toLowerCase() === 'quit') {
+                console.log('Game Over\n');
+                readLine.close();
+                return;
+            }
+            
+            if (trimmedUserInput) getFullCountryName(trimmedUserInput);
+            else console.log('Please enter valid country code!')
+            console.log('');
+            takeUserChoice();
+        });
+    }
+  
+    takeUserChoice();
+}
+
+main();
