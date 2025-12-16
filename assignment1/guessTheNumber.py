120eeae0910ef6220110b405d1b61d206456abf8
Ajmat Kathat <ajmatkathat@Ajmat-MacBook-Pro.local>
2025-12-16 16:47:44 +0530

    Completed Learn and code Assignment 1

---
+++ assignment1/guessTheNumber.py
@@ -0,0 +1,33 @@
+import random
+
+def isValidGuess(userGuessValue):
+    if userGuessValue.isdigit() and 1 <= int(userGuessValue) <= 100:
+        return True
+    else:
+        return False
+
+
+def main():
+    secretNumberToGuess = random.randint(1, 100)
+    isGuessedCorrectly = False
+    userGuessValue = input("Guess a number between 1 and 100:")
+    totalGuessCount = 0
+
+    while not isGuessedCorrectly:
+        if not isValidGuess(userGuessValue):
+            userGuessValue = input("I wont count this one Please enter a number between 1 to 100")
+            continue
+        else:
+            totalGuessCount += 1
+            userGuessValue = int(userGuessValue)
+
+        if userGuessValue < secretNumberToGuess:
+            userGuessValue = input("Too low. Guess again")
+        elif userGuessValue > secretNumberToGuess:
+            userGuessValue = input("Too High. Guess again")
+        else:
+            print("You guessed it in", totalGuessCount, "guesses!")
+            isGuessedCorrectly = True
+
+
+main()
